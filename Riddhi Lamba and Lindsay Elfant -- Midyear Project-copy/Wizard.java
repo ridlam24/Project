e0f0bdedf8a216d310e03be500f83660244f6e03
Riddhi Lamba <ridlam24@bergen.org>
2023-02-14 09:44:48 -0500

    hi

--- Riddhi Lamba and Lindsay Elfant -- Midyear Project-copy/Wizard.java
+++ Riddhi Lamba and Lindsay Elfant -- Midyear Project-copy/Wizard.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Wizard extends Actor
+public abstract class Wizard extends Actor
 {
     private int health; 
 

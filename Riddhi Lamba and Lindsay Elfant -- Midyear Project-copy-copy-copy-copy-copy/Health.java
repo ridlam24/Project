86743b8ae9645f6c961204634f9de4af941ef240
Lindsay Elfant <liluelfant@gmail.com>
2023-03-03 09:27:06 -0500

    a;ldkjf; alskdfj

--- Riddhi Lamba and Lindsay Elfant -- Midyear Project-copy-copy-copy-copy-copy/Health.java
+++ Riddhi Lamba and Lindsay Elfant -- Midyear Project-copy-copy-copy-copy-copy/Health.java
@@ -23,14 +23,7 @@ public class Health extends Actor
     public void changeHealthBar(int health) {
         GreenfootImage image = getImage(); 
         int amount = (health/100)* originalWidth; 
-    
-        if (health > 0) {            
-            image.scale(image.getWidth() + amount, image.getHeight());
-        }
-        else {
-            
-            image.scale(image.getWidth() - amount, image.getHeight()); 
-        }
+        image.scale(image.getWidth() - amount, image.getHeight()); 
     }
 
     public void act()

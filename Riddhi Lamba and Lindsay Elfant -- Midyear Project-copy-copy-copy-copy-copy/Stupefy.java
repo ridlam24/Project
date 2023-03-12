e0ff49ac20ed38f44406b6b8f54fd9856f226c8e
Lindsay Elfant <liluelfant@gmail.com>
2023-03-11 19:12:53 -0500

    stupefy randomize

--- Riddhi Lamba and Lindsay Elfant -- Midyear Project-copy-copy-copy-copy-copy/Stupefy.java
+++ Riddhi Lamba and Lindsay Elfant -- Midyear Project-copy-copy-copy-copy-copy/Stupefy.java
@@ -23,6 +23,7 @@ public class Stupefy extends Offensive
     }
 
     public int randomDamage() {
-        return damage; 
+        int random = -1 * (1 + Greenfoot.getRandomNumber(18)); 
+        return random; 
     }
 }
